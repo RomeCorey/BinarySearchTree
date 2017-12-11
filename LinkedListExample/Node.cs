0fffbd8cbb7ab00f85b9f3a337e0fb4e3dbd171c
Corey Rome <coreyrome2@gmail.com>
2017-12-11 15:06:49 -0600

    initial commit

---
+++ LinkedListExample/Node.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListExample
+{
+    class Node
+    {
+        public Node left;
+        public Node right;
+
+        public Node()
+        {
+
+        }
+
+        public Node(int initial)
+        {
+            value = initial;
+            left = null;
+            right = null;
+        }
+    }
+
+    class Tree
+    {
+        Node top;
+
+        public Tree()
+        {
+            top = null;
+        }
+
+        public Tree(int initial)
+        {
+            top = new Node(initial);
+        }
+
+
+        public void Add(int value)
+        {
+
+            if (top == null)
+            {
+                Node NewNode = new Node(value);
+                top = NewNode;
+                return;
+            }
+            Node currentNode = top;
+            bool added = false;
+            do
+            {
+                if (value < currentNode.value)
+                {
+                    if (currentNode.left == null)
+                    {
+                        Node NewNode = new Node(value);
+                        currentNode.left = NewNode;
+                        added = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.left;
+                    }
+                }
+                if (value >= currentNode.value)
+                {
+                    if (currentNode.right == null)
+                    {
+                        Node NewNode = new Node(value);
+                        currentNode.right = NewNode;
+                        added = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.right;
+                    }
+                }
+            }
+     }
+    
+       
+    
+
+
+
+
+
+
+// binary search tree
+// add method
+// search method that will also track the path
---
+++ LinkedListExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListExample
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, bool> foo = new Dictionary<string, bool>();
+            foo.Add("name", true);
+            bool fee = foo["name"];
+            foreach(KeyValuePair<string, bool> entry in foo)
+            {
+                Console.WriteLine(entry.Key, entry.Value);
+            }
+        }
+
+    }
+}
